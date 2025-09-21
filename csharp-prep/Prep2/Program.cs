811813df0ca62ad56412423e463d1742a6e5a5c0
teaguerchubak <teaguechubak@gmail.com>
2025-09-20 21:08:03 -0600

    Completed prep1 and prep2 assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeString = Console.ReadLine();
+        int gradeNumber = int.Parse(gradeString);
+        string letter;
+
+        if (gradeNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your final grade: {letter} ");
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass this time. Please try again!");
+        }
     }
 }
